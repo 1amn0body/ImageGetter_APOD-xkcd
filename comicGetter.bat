b03fb3caa1a69d784e78e2f440c00822e7e79f25
1amn0body <firewolf6342@gmail.com>
2020-08-12 00:15:52 +0200

    finishing

---
+++ comicGetter.bat
@@ -0,0 +1,9 @@
+@echo on
+if exist imgs (
+    rmdir /q /s imgs
+)
+
+mkdir imgs
+cd imgs
+
+python ..\getImage.py
--- getImage.py
+++ getImage.py
@@ -1,8 +1,7 @@
 import requests
 from bs4 import BeautifulSoup as bs
-#from os.path import expanduser, exists
 from os.path import basename
-#from pathlib import Path
+from os import mkdir
 
 url = "https://xkcd.com/"
 html_doc = requests.get(url).text
@@ -12,16 +11,7 @@ soup = bs(html_doc, 'html.parser')
 #print(soup.prettify()) #print whole html document prettified
 
 imgLink = soup.find(id="comic").find('img')['src']
-print(imgLink)
-
-#userPath = expanduser("~")
-#filePath = Path("Desktop")
-
-#print(Path.cwd()) #now active path / path of this file
-
-#save img
-#with open("Y:\\ERIK\\Informatik\\Python\\xkcdComicGetter", "wb") as dir:
-#    dir.write(requests.get("http:" + imgLink).content)
+#print(imgLink)
 
 with open(basename(imgLink), "wb") as dir:
     dir.write(requests.get("http:" + imgLink).content)
