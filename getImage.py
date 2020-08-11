a14e5373cfda870b6bf412f23b503c76f8733ae2
1amn0body <firewolf6342@gmail.com>
2020-08-11 22:41:19 +0200

    coding

--- getImage.py
+++ getImage.py
@@ -1,11 +1,23 @@
 import requests
 from bs4 import BeautifulSoup as bs
+from os.path import expanduser
+from os.path import basename
+from os import sep
 
 url = "https://xkcd.com/"
 html_doc = requests.get(url).text
 
 soup = bs(html_doc, 'html.parser')
 
-print(soup.find(id="comic").find('img')['src'])
+#print(soup.prettify()) #print whole html document prettified
 
-#print(soup.prettify())
+imgLink = soup.find(id="comic").find('img')['src']
+print(imgLink)
+
+home = expanduser("~")
+print(home)
+
+#with open(home +  "" sep + "", "wb") as dir:
+#    dir.write(requests.get("http:" + imgLink).content)
+with open(basename(imgLink), "wb") as dir:
+    dir.write(requests.get(imgLink).content)
