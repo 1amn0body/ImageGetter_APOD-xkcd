dabc258eed3462bc88f6b5bcd6da8d576cdfa55f
1amn0body <firewolf6342@gmail.com>
2020-08-12 18:06:19 +0200

    get 3 comics

--- getImage.py
+++ getImage.py
@@ -3,42 +3,40 @@ from bs4 import BeautifulSoup as bs
 from os.path import basename
 from os import mkdir
 
+def saveComic(theSoup):
+    imgLnk = theSoup.find(id="comic").find('img')['src']
+    print("Comic from https:" + imgLnk)
+
+    with open(basename(imgLnk), "wb") as dir:
+        dir.write(requests.get("https:" + imgLnk).content)
+
+#print(soup.prettify()) #print whole html document prettified
+
 url = "https://xkcd.com/"
 html_doc = requests.get(url).text
 
 soup = bs(html_doc, 'html.parser')
 
-#print(soup.prettify()) #print whole html document prettified
+saveComic(soup)
 
-imgLink = soup.find(id="comic").find('img')['src']
-#print(imgLink)
-
-with open(basename(imgLink), "wb") as dir:
-    dir.write(requests.get("http:" + imgLink).content)
-    
+#########
 #get 2 older comics
-prevLink = soup.find('ul' class="comicNav").find('a' rel="prev")['href']
+prevLink = soup.find('ul', {'class': ["comicNav"]}).find('a', {'rel': ["prev"]})['href']
 rePrevLink = int(prevLink.replace('/', ''))
 
 
 #soup2
-url2 = ("https://xkcd.com/" + rePrevLink)
+url2 = ("https://xkcd.com/" + str(rePrevLink))
 html_doc2 = requests.get(url2).text
 
 soup2 = bs(html_doc2, 'html.parser')
 
-imgLink2 = soup2.find(id="comic").find('img')['src']
-
-with open(basename(imgLink2), "wb") as dir:
-    dir.write(requests.get("http:" + imgLink2).content)
+saveComic(soup2)
 
 #soup3
-url3 = ("https://xkcd.com/" + (rePrevLink - 1))
+url3 = ("https://xkcd.com/" + str(rePrevLink - 1))
 html_doc3 = requests.get(url3).text
 
 soup3 = bs(html_doc3, 'html.parser')
 
-imgLink3 = soup3.find(id="comic").find('img')['src']
-
-with open(basename(imgLink2), "wb") as dir:
-    dir.write(requests.get("http:" + imgLink2).content)
+saveComic(soup3)
